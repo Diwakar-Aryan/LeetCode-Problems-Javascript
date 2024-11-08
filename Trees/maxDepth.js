ab1d4131de40f5dd0814b408513bbeafc0c995da
Diwakar Singh <diwakar@heady.io>
2024-11-08 17:36:12 +0530

    Max Depth of binary tree

---
+++ Trees/maxDepth.js
@@ -0,0 +1,30 @@
+const Node = require("./Node");
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+
+  let lh = maxDepth(root.left);
+  let rh = maxDepth(root.right);
+
+  return 1 + Math.max(lh, rh);
+};
+
+const root = new Node(1)
+root.left = new Node(2);
+root.right = new Node(3);
+root.right.left = new Node(4);
+root.right.right = new Node(6);
+root.right.left.left = new Node(5)
+
+console.log(maxDepth((root)))
